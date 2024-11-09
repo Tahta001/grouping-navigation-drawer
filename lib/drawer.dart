68da3c4a367873ad2286b318264e1baf21f80001
Tahta001 <tahta.xpplg2@gmail.com>
2024-11-09 08:36:55 +0700

    seccond_commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -15,104 +15,89 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         foregroundColor: Colors.white,
       ),
-      drawer: buildGrupDrawer(context),
-      body: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          shrinkWrap: true,
-          children: List.generate(10, (index) {
-            return Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Container(
-                    height: 5.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.blue[900]),
-                    child: Text(
-                      'materi$index',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                    )));
-          })),
+      drawer: buildGroupDrawer(context),
+      body: Center(
+        child: Text("Pemrograman Mobile $title"),
+      ),
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   crossAxisSpacing: 10.0,
+      //   mainAxisSpacing: 10.0,
+      //   shrinkWrap: true,
+      //   children: List.generate(10, (index){
+      //     return Padding(
+      //       padding: EdgeInsets.all(10.0),
+      //       child: Container(
+      //         height: 5.0,
+      //         decoration: BoxDecoration(
+      //           borderRadius: BorderRadius.circular( 20.0),
+      //           color: Colors.blue[900]
+      //         ),
+      //           child: Text ('materi$index',
+      //           textAlign: TextAlign.center,
+      //           style: TextStyle(color: Colors.black, fontSize: 20,),)
+      //     )
+      //     );
+      //   })
+      // ),
     );
   }
 
-  Widget buildGrupDrawer(BuildContext context) {
+  Widget buildGroupDrawer(context) {
     return Drawer(
       child: ListView(padding: EdgeInsets.zero, children: <Widget>[
         DrawerHeader(
           decoration: BoxDecoration(color: Colors.blue[900]),
-          child: Text('Tahta Nurul Ariyanti',
-              style: TextStyle(color: Colors.white, fontSize: 30)),
+          child: UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue[900]),
+            accountName: Text(
+              "Tahta Nurul Ariyanti",
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            accountEmail: Text("tahta.xpplg2@gmail.com"),
+            currentAccountPictureSize: Size.square(40),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Text(
+                "T",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ),
+          //     child: Text('Rahmalia Putri Pratiwi', style: TextStyle(color: Colors.white, fontSize: 30)),
+          // ),
+          // ExpansionTile(
+          //   title: const Text('Materi'),
+          //   children: [
+          //     ListTile(title: Text('Materi 1'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Materi 2'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Materi 3'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Materi 4'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Materi 5'), onTap: (){Navigator.pop(context);},),
+          //   ]
+          // ),
+          // ExpansionTile(
+          //   title: const Text('Tugas'),
+          //   children: [
+          //     ListTile(title: Text('Tugas 1'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Tugas 2'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Tugas 3'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Tugas 4'), onTap: (){Navigator.pop(context);},),
+          //     ListTile(title: Text('Tugas 5'), onTap: (){Navigator.pop(context);},),
+          //   ]
+          //
         ),
-        ExpansionTile(title: const Text('Materi'), children: [
-          ListTile(
-            title: Text('Materi 1'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Materi 2'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Materi 3'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Materi 4'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Materi 5'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-        ]),
-        ExpansionTile(title: const Text('Tugas'), children: [
-          ListTile(
-            title: Text('Tugas 1'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Tugas 2'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Tugas 3'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Tugas 4'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Tugas 5'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-        ])
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("Tahta"),
+          subtitle: Text("XII RPL 2"),
+          trailing: Icon(Icons.chevron_right),
+        )
       ]),
     );
   }
